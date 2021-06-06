a9b7d19d6075068807384d56d4f3e33dc7dfc8bb
Sandeep Singh <sandeep@projectdiscovery.io>
2021-06-06 16:01:07 +0530

    Update leak.go

--- test/leak.go
+++ test/leak.go
@@ -1,1 +1,24 @@
-AKIAIO5FODNN7EXAMPLE
+	"github.com/projectdiscovery/goflags"
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/nuclei/v2/internal/runner"
+	"github.com/projectdiscovery/nuclei/v2/pkg/types"
+)
+
+var (
+	cfgFile string
+	options = &types.Options{"AKIAIO5FODNN7EXAMPLE"}
+)
+
+func main() {
+	readConfig()
+
+	runner.ParseOptions(options)
+
+	nucleiRunner, err := runner.New(options)
+	if err != nil {
+		gologger.Fatal().Msgf("Could not create runner: %s\n", err)
+	}
+	nucleiRunner.RunEnumeration()
+}
+
+
